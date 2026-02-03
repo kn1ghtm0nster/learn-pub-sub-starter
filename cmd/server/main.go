3d2055c50f88a8f0dd85c673c3354d9fc7c1e489
kn1ghtm0nster <diego.quintanilla@proton.me>
2026-02-02 20:24:49 -0500

    CH2-L4: add connection logic to code and install deps

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	connStr := "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(connStr)
+	if err != nil {
+		fmt.Println("Failed to connect to RabbitMQ:", err)
+		return
+	}
+	defer conn.Close()
+
 	fmt.Println("Starting Peril server...")
+	fmt.Println("Connected to RabbitMQ successfully!")
+
+	// use signal to wait for termination
+	// after user presses Ctrl+C, print a message and exit
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	<-sigChan
+	fmt.Println("Shutting down Peril server...")
 }
