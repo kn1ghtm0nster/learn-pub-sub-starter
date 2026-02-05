d2d6f4b22ea3125f68ee28c0ae3db72919f960f0
kn1ghtm0nster <diego.quintanilla@proton.me>
2026-02-04 22:05:59 -0500

    CH3-L1: add logic for publishing pause messages to consumers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -13,13 +16,33 @@ func main() {
 
 	conn, err := amqp.Dial(connStr)
 	if err != nil {
-		fmt.Println("Failed to connect to RabbitMQ:", err)
+		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
 		return
 	}
-	defer conn.Close()
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Failed to open a channel: %v", err)
+		return
+	}
 
+	defer ch.Close()
+	defer conn.Close()
 	fmt.Println("Starting Peril server...")
 	fmt.Println("Connected to RabbitMQ successfully!")
+
+	err = pubsub.PublishJSON(
+		ch,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		},
+	)
+	if err != nil {
+		log.Fatalf("Failed to publish message: %v", err)
+		return
+	}
+
 
 	// use signal to wait for termination
 	// after user presses Ctrl+C, print a message and exit
